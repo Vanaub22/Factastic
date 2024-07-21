5c656733e9356c8962034bb1472b32300aab2baa
Vanaub22 <anucbs2018@gmail.com>
2024-07-22 01:48:32 +0530

    Hid supabase url and api key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://qzbpxovadfxxzywuzjff.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6YnB4b3ZhZGZ4eHp5d3V6amZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEyNDU4MjcsImV4cCI6MjAzNjgyMTgyN30.X8MOLRL6faS0EUjkBgZGyR8AhiLLtgboJbOyYESjMA0';
+const supabaseUrl = '<your_url_as_string>';
+const supabaseKey = 'your_key_as_string';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
